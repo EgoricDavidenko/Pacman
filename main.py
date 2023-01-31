12d13d355023af0456bf03000531808390d03f54
exzample <arseny.ackimov@yandex.ru>
2023-01-31 18:29:40 +0300

    full pacman

--- main.py
+++ main.py
@@ -1,40 +1,84 @@
+import copy
+from level1 import boards
 import pygame
-import Player
+import math
+import player
+
+pygame.init()
+
+WIDTH = 900
+HEIGHT = 1000
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+timer = pygame.time.Clock()
+fps = 120
+level = copy.deepcopy(boards)
+PI = math.pi
+
+flicker = False
+
+player = player.Player(450, 663)
 
 all_sprites = pygame.sprite.Group()
 
-if __name__ == '__main__':
-    pygame.init()
-    size = width, height = 600, 400
-    screen = pygame.display.set_mode(size)
-    pygame.display.set_caption('pacman')
+all_sprites.add(player)
 
-    fps = 30
-    running = True
-    clock = pygame.time.Clock()
+print(level[25][27])
 
-    player = Player.Player(20, 100, 100)
-    all_sprites.add(player)
+def draw_board():
+    rows = 33
+    cols = 30
+    for i in range(rows):
+        for j in range(cols):
+            if boards[i][j] == 1:
+                pygame.draw.circle(screen, 'white', (j * 30 + (0.5 * 30), i * 30 + (0.5 * 30)), 4)
+            if boards[i][j] == 2 and not flicker:
+                pygame.draw.circle(screen, 'white', (j * 30 + (0.5 * 30), i * 30 + (0.5 * 30)), 10)
+            if boards[i][j] == 3:
+                pygame.draw.line(screen, 'blue', (j * 30 + (0.5 * 30), i * 30),
+                                 (j * 30 + (0.5 * 30), i * 30 + 30), 3)
+            if boards[i][j] == 4:
+                pygame.draw.line(screen, 'blue', (j * 30, i * 30 + (0.5 * 30)),
+                                 (j * 30 + 30, i * 30 + (0.5 * 30)), 3)
+            if boards[i][j] == 5:
+                pygame.draw.arc(screen, 'blue', [(j * 30 - (30 * 0.4)) - 2, (i * 30 + (0.5 * 30)), 30, 30],
+                                0, PI / 2, 3)
+            if boards[i][j] == 6:
+                pygame.draw.arc(screen, 'blue',
+                                [(j * 30 + (30 * 0.5)), (i * 30 + (0.5 * 30)), 30, 30], PI / 2, PI, 3)
+            if boards[i][j] == 7:
+                pygame.draw.arc(screen, 'blue', [(j * 30 + (30 * 0.5)), (i * 30 - (0.4 * 30)), 30, 30], PI,
+                                3 * PI / 2, 3)
+            if boards[i][j] == 8:
+                pygame.draw.arc(screen, 'blue',
+                                [(j * 30 - (30 * 0.4)) - 2, (i * 30 - (0.4 * 30)), 30, 30], 3 * PI / 2,
+                                2 * PI, 3)
+            if boards[i][j] == 9:
+                pygame.draw.line(screen, 'white', (j * 30, i * 30 + (0.5 * 30)),
+                                 (j * 30 + 30, i * 30 + (0.5 * 30)), 3)
 
-    while running:
-        clock.tick(fps)
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    player.move("up")
-                if event.key == pygame.K_DOWN:
-                    player.move("down")
-                if event.key == pygame.K_LEFT:
-                    player.move("left")
-                if event.key == pygame.K_RIGHT:
-                    player.move("right")
 
-        screen.fill((0, 0, 0))
-        all_sprites.draw(screen)
-        all_sprites.update()
-        pygame.display.flip()
-    pygame.quit()
-    #zxc
+run = True
+while run:
+    timer.tick(fps)
+
+    screen.fill('black')
+    draw_board()
+    all_sprites.update()
+    all_sprites.draw(screen)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                player.move("up")
+            if event.key == pygame.K_DOWN:
+                player.move("down")
+            if event.key == pygame.K_LEFT:
+                player.move("left")
+            if event.key == pygame.K_RIGHT:
+                player.move("right")
+
+    pygame.display.flip()
+pygame.quit()
---
+++ player.py
@@ -0,0 +1,134 @@
+import pygame
+import  level1
+from level1 import boards
+import copy
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+
+        self.level = copy.deepcopy(boards)
+
+        self.player_images = []
+        for i in range(1, 17):
+            self.player_images.append(pygame.transform.scale(pygame.image.load(f'player_images/{i}.png'), (30, 30)))
+
+        # self.image = pygame.Surface((2 * 20, 2 * 20),
+        #                             pygame.SRCALPHA, 32)
+        # pygame.draw.circle(self.image, pygame.Color("yellow"),
+        #                    (20, 20), 20)
+        self.image = self.player_images[0]
+
+        self.rect = pygame.Rect(x, y, 40, 40)
+
+        # self.rect.x = x
+        # self.rect.y = y
+
+        self.mx = 15 # !!! normal cords in our opinion -1
+        self.my = 24
+        self.partx = 0
+        self.party = 0
+
+        self.dir = "right"
+        self.prev_dir = "right"
+        self.start_game = False
+        self.animation_counter = 0
+        self.flicker = False
+        self.moving = "stop"
+
+        self.counter = 3
+
+    def move(self, direction):
+        self.start_game = True
+        self.prev_dir = self.dir
+        self.dir = direction
+
+
+    def update(self):
+        self.rect.x, self.rect.y = self.coord_to_pos(self.mx, self.my)
+
+        if self.start_game:
+
+            if self.counter % 3 == 0:
+                self.counter = 0
+                if self.dir == "up":
+                    if self.level[self.my - 1][self.mx] in [0, 1, 2]:
+                        self.prev_dir = "up"
+                        if self.level[self.my][self.mx] == 1 or self.level[self.my][self.mx] == 2:
+                            level1.boards[self.my][self.mx] = 0
+                        self.party -= 1
+                        self.moving = "up"
+                    else:
+                        self.dir = self.prev_dir
+                if self.dir == "down":
+                    if self.level[self.my + 1][self.mx] in [0, 1, 2]:
+                        self.prev_dir = "down"
+                        if self.level[self.my][self.mx] == 1 or self.level[self.my][self.mx] == 2:
+                            level1.boards[self.my][self.mx] = 0
+                        self.party += 1
+                        self.moving = "down"
+                    else:
+                        self.dir = self.prev_dir
+                if self.dir == "left":
+                    if self.level[self.my][self.mx - 1] in [0, 1, 2]:
+                        self.prev_dir = "left"
+                        if self.level[self.my][self.mx] == 1 or self.level[self.my][self.mx] == 2:
+                            level1.boards[self.my][self.mx] = 0
+                        self.partx -= 1
+                        self.moving = "left"
+                    else:
+                        self.dir = self.prev_dir
+                if self.dir == "right":
+                    if self.level[self.my][self.mx + 1] in [0, 1, 2]:
+                        self.prev_dir = "rught"
+                        if self.level[self.my][self.mx] == 1 or self.level[self.my][self.mx] == 2:
+                            level1.boards[self.my][self.mx] = 0
+                        self.partx += 1
+                        self.moving = "right"
+                    else:
+                        self.dir = self.prev_dir
+            self.counter += 1
+
+        self.rect.x, self.rect.y = self.coord_to_pos(self.mx, self.my)
+        # print(self.coord_to_pos(self.mx, self.my))
+
+        if self.animation_counter < 19:
+            self.animation_counter += 1
+            if self.animation_counter > 3:
+                self.flicker = False
+        else:
+            self.animation_counter = 0
+            self.flicker = True
+
+        self.draw_player()
+
+    def draw_player(self):
+        # 0-RIGHT, 1-LEFT, 2-UP, 3-DOWN
+        if self.dir == "right":
+            self.image = self.player_images[self.animation_counter // 5]
+        elif self.dir == "left":
+            self.image = self.player_images[self.animation_counter // 5 + 8]
+        elif self.dir == "up":
+            self.image = self.player_images[self.animation_counter // 5 + 12]
+        elif self.dir == "down":
+            self.image = self.player_images[self.animation_counter // 5 + 4]
+
+    def coord_to_pos(self, mx, my):
+        if self.partx == 6:
+            self.mx += 1
+            mx += 1
+            self.partx = 0
+        elif self.partx == -6:
+            self.mx -= 1
+            mx -= 1
+            self.partx = 0
+        if self.party == 6:
+            self.my += 1
+            my += 1
+            self.party = 0
+        if self.party == -6:
+            self.my -= 1
+            my -= 1
+            self.party = 0
+
+        return mx * 30 + self.partx * 5, my * 30 + self.party * 5
